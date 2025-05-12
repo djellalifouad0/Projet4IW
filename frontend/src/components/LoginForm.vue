5b3ae8ba0898e6935d4e3fb2674bebb2fb081746
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2025-05-12 20:06:41 +0200

    Front start/login/register

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login-left">
+        <img src="../assets/images/SkillSwap Logo.png" alt="SkillSwap" class="login-logo" />
+        <span class="login-logo-title">SkillSwap</span>
+    </div>
+    <div class="login-box">
+      <h2>Bienvenue sur SkillSwap</h2>
+      <div class="switch-link">
+        <span>Pas de compte ?</span> <br />
+        <router-link to="/register">Inscrivez-vous</router-link>
+      </div>
+      <h1>Connectez-vous</h1>
+      <form @submit.prevent="handleLogin">
+        <input v-model="email" type="email" placeholder="Adresse e-mail" required />
+        <div class="password-field">
+          <input v-model="password" :type="showPassword ? 'text' : 'password'" placeholder="Mot de passe" required />
+          <button type="button" @click="showPassword = !showPassword">👁️</button>
+        </div>        <router-link to="/forgot-password" class="forgot">Mot de passe oublié</router-link>
+        <button type="submit" class="primary">Se connecter</button>
+      </form>
+      <div class="divider">OU</div>
+      <button class="google-login">Se connecter avec Google</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  components: {
+    RouterLink
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      showPassword: false,
+    };
+  },
+  methods: {
+    handleLogin() {
+      console.log('Connexion avec', this.email, this.password);
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/RegisterForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login-left">
+      <img src="../assets/images/SkillSwap Logo.png" alt="SkillSwap" class="login-logo" />
+      <span class="login-logo-title">SkillSwap</span>
+    </div>
+    <div class="login-box">
+      <h2>Bienvenue sur SkillSwap</h2>
+      <div class="switch-link">
+        <span>Déjà un compte ?</span>
+        <router-link to="/login">Connectez-vous</router-link>
+      </div>
+      <h1>Inscrivez-vous</h1>
+      <form @submit.prevent="handleRegister">
+        <div class="flex-row">
+          <input v-model="email" type="email" placeholder="adresse e-mail" required />
+          <input v-model="phone" type="tel" placeholder="Numéro de téléphone" required />
+        </div>
+        <div class="flex-row">
+          <input v-model="firstName" type="text" placeholder="Prénom" required />
+          <input v-model="lastName" type="text" placeholder="Nom" required />
+        </div>
+        <input v-model="password" type="password" placeholder="Mot de passe" required />
+        <button type="submit" class="primary">S'inscrire</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  components: {
+    RouterLink
+  },
+  data() {
+    return {
+      email: '',
+      phone: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleRegister() {
+      console.log('Inscription avec', this.email);
+    },
+  },
+};
+</script>
